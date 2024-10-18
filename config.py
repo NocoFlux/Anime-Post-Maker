05455eb8556ebfbedf69f5d7e59cc95128dd199b
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-18 16:20:44 +0600

    Update

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")  # Bot token from environment variable
+TG_OWNER_ID = os.environ.get("TG_OWNER_ID", "")    # Owner ID from environment variable
+TG_CHANNEL_1 = os.environ.get("TG_CHANNEL_1", "")
+TG_CHANNEL_2 = os.environ.get("TG_CHANNEL_2", "")
+TG_CHANNEL_3 = os.environ.get("TG_CHANNEL_3", "")
