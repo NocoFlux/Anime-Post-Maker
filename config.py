1d29c4128f44f1d2a58914397fbf7087ab353ee3
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-19 23:52:57 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+#https://t.me/abidabdullah199
+
 import os
 
-BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002191732189"))
+#Bot Variables
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "bot token telegram")
+APP_ID = int(os.environ.get("APP_ID", "telegram app id"))
+API_HASH = os.environ.get("API_HASH", "telegram API hash")
+OWNER_ID = int(os.environ.get("OWNER_ID", "telegram owner Id 5296584067"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "my telegram channel id"))
 API_URL =
-os.environ.get("API_URL", "")
+os.environ.get("API_URL", "API url for getting anime news")
