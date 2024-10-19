18e2b0f68d09bf9c56c9659a1106b8ed834c681c
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-19 23:43:56 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 
 BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002191732189"))
-API_URL = os.environ.get("API_URL", "")
+API_URL =
+os.environ.get("API_URL", "")
