6ec3d6434ca6775dd34fe708de6fec5bd1baf6f9
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-20 17:04:51 +0600

    Update config.py

--- config.py
+++ config.py
@@ -27,3 +27,24 @@ query {
   }
 }
 """
+
+LOG_FILE_NAME = "tg-anime-news-bot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
