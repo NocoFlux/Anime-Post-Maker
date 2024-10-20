29f9192b46ab0fd0b22d23b903f44e852e8ca0bb
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-20 11:03:58 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,16 @@
-from telegram import Update
-from telegram.ext import CallbackContext
+from pyrogram import Client, filters
+from helper_func import get_readable_time
+from datetime import datetime
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Welcome to the Post Maker Bot! Use /create to start creating a post.')
+@Client.on_message(filters.command("start") & filters.private)
+async def start_cmd(client, message):
+    uptime = get_readable_time((datetime.now() - client.uptime).total_seconds())
+    await message.reply(f"Bot is alive!\nUptime: {uptime}")
+
+@Client.on_message(filters.command("ping"))
+async def ping_cmd(client, message):
+    start = datetime.now()
+    reply = await message.reply("Pinging...")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await reply.edit(f"Pong!\nResponse Time: `{ms}ms`")
