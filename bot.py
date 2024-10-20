689cffcce37c85752aa4fde3242c41395205fa38
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-20 10:53:47 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+from pyrogram import Client
+from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={
+                "root": "plugins"
+            },
+            bot_token=BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+        try:
+            await self.send_message(CHANNEL_ID, "⚡️ Bot is alive")
+            self.LOGGER.info(f"✅ Bot has access to channel {CHANNEL_ID}")
+        except Exception as e:
+            self.LOGGER.error(f"❌ Bot lacks channel access: {str(e)}")
+        
+        self.LOGGER.info(f"Bot Started as {usr_bot_me.first_name}")
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER.info("Bot Stopped!")
+      
